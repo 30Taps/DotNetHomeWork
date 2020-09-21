c97399bc3278456698384240d283d4fae5554c6a
30Taps <1241749920@qq.com>
2020-09-21 15:46:11 +0800

    finish

---
+++ homework1/The Sieve of Eratosthenes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace The_Sieve_of_Eratosthenes
+{
+    public struct Array
+    {
+        public int num;
+        public bool isPrime;
+    }
+
+    class Program
+    {
+        static int MAX = 100;
+        static void Sieve(Array[] list)
+        {
+            list[0].isPrime = false;
+            list[1].isPrime = false;
+            for(int i=2; i<MAX;i++)
+            {
+                if(list[i].isPrime)
+                {
+                    //Console.WriteLine(list[i].num);
+                    for(int j=2; i*j<MAX;j++)
+                    {
+                        list[i * j].isPrime = false;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Array[] list = new Array[MAX];
+            for(int i=0; i<MAX;i++)
+            {
+                list[i].num = i;
+                list[i].isPrime = true;
+            }
+            Sieve(list);
+            Console.WriteLine($"MAX={MAX}");
+            for(int i=0; i<MAX;i++)
+            {
+                if(list[i].isPrime)
+                {
+                    Console.Write(list[i].num + " ");
+                }
+            }
+        }
+    }
+}
